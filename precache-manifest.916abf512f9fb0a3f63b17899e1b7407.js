6ceb0138dad0c231f154b0b4e44132f7631ea383
Or Partush <orptzz@gmail.com>
2020-04-28 17:53:16 +0300

    Updates

---
+++ precache-manifest.916abf512f9fb0a3f63b17899e1b7407.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56914622c459ed465d4ec039e551ccc2",
+    "url": "/mr-bitcoin/index.html"
+  },
+  {
+    "revision": "c234ea4838466edfffcb",
+    "url": "/mr-bitcoin/static/css/main.2ac07087.chunk.css"
+  },
+  {
+    "revision": "138c9adc06a6bae0d049",
+    "url": "/mr-bitcoin/static/js/2.c19363b0.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/mr-bitcoin/static/js/2.c19363b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c234ea4838466edfffcb",
+    "url": "/mr-bitcoin/static/js/main.c9c7b2a1.chunk.js"
+  },
+  {
+    "revision": "73e654ec2f0cb3185f9d",
+    "url": "/mr-bitcoin/static/js/runtime-main.2af77c22.js"
+  },
+  {
+    "revision": "8d99c45d7a22b18a43383fc07e6d1c72",
+    "url": "/mr-bitcoin/static/media/AlegreyaSans-ExtraBold.8d99c45d.ttf"
+  },
+  {
+    "revision": "faa3c83d580611cb766fbedafddedf21",
+    "url": "/mr-bitcoin/static/media/AlegreyaSans-Regular.faa3c83d.ttf"
+  },
+  {
+    "revision": "fb7e3a294cb07a54605a8bb27f0cd528",
+    "url": "/mr-bitcoin/static/media/OpenSans-ExtraBold.fb7e3a29.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/mr-bitcoin/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "be3bf63a30b4523ae221bd4358b13e8f",
+    "url": "/mr-bitcoin/static/media/Raleway-ExtraBold.be3bf63a.ttf"
+  },
+  {
+    "revision": "bb5ae98e4ce1a64042093dc235c305ed",
+    "url": "/mr-bitcoin/static/media/Raleway-Medium.bb5ae98e.ttf"
+  },
+  {
+    "revision": "81fb83aa58d464865109ba690dbfac52",
+    "url": "/mr-bitcoin/static/media/bitcoin.81fb83aa.png"
+  }
+]);
